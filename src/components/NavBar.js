356b4fb45a5a909ab83823982eb9135ceb934076
cDev-Creator <mccoyng170@gmail.com>
2023-03-15 15:40:56 -0500

    dropdown nav closes automatically when new link selected

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,37 +1,75 @@
-import {Container, Nav, Navbar} from 'react-bootstrap';
-import {BrowserRouter as NavLink} from "react-router-dom";
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
 
 export const NavBar = () => {
-    return (
-      <Navbar collapseOnSelect expand="lg">
-        <Container>
-          <Navbar.Brand>
-            <h1 style={{fontSize:'30px'}}>Sober Landing</h1>
-          </Navbar.Brand>
-            
-          <Navbar.Toggle aria-controls="responsive-navbar-nav bg-dark" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ms-auto">
-              <NavLink exact to="/home" activeClassName="active">
-                Home
-              </NavLink>
-              <NavLink to="/about" activeClassName="active">
-                About
-              </NavLink>
-              <NavLink to="/locations" activeClassName="active">
-                Our House
-              </NavLink>
-              <NavLink to="/contact" activeClassName="active">
-                Contact
-              </NavLink>
-              <NavLink to="/faq" activeClassName="active">
-                FAQ
-              </NavLink>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  const [expanded, setExpanded] = useState(false);
+
+  const handleSelect = () => {
+    setExpanded(false);
   };
-  
-  export default NavBar;
+
+  return (
+    <Navbar
+      collapseOnSelect
+      expand="lg"
+      expanded={expanded}
+    >
+      <Container>
+        <Navbar.Brand>
+          <h1 style={{fontSize:'30px'}}>Sober Landing</h1>
+        </Navbar.Brand>
+          
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav bg-dark"
+          onClick={() => setExpanded(!expanded)}
+        />
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          onSelect={handleSelect}
+        >
+          <Nav className="ms-auto">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              onClick={() => setExpanded(false)}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about"
+              activeClassName="active"
+              onClick={() => setExpanded(false)}
+            >
+              About
+            </NavLink>
+            <NavLink
+              to="/locations"
+              activeClassName="active"
+              onClick={() => setExpanded(false)}
+            >
+              Our House
+            </NavLink>
+            <NavLink
+              to="/contact"
+              activeClassName="active"
+              onClick={() => setExpanded(false)}
+            >
+              Contact
+            </NavLink>
+            <NavLink
+              to="/faq"
+              activeClassName="active"
+              onClick={() => setExpanded(false)}
+            >
+              FAQ
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
