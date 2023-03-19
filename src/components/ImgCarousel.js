c936a3c50330f9ecc6529937a58f4e176e5310ef
cDev-Creator <mccoyng170@gmail.com>
2023-03-19 12:34:08 -0500

    photos of the home added to img carousel

--- src/components/ImgCarousel.js
+++ src/components/ImgCarousel.js
@@ -3,8 +3,15 @@ import Carousel from 'react-bootstrap/Carousel';
 import '../index.css';
 
 
-import house from '../assets/images/examplehouse.jpg';
-import swan from '../assets/images/swan.png';
+import house1 from '../assets/images/1.jpg';
+import house2 from '../assets/images/2.jpg';
+import house3 from '../assets/images/3.jpg';
+import house4 from '../assets/images/4.jpg';
+import house5 from '../assets/images/5.jpg';
+import house6 from '../assets/images/6.jpg';
+import house7 from '../assets/images/7.jpg';
+import house8 from '../assets/images/8.jpg';
+import house9 from '../assets/images/9.jpg';
 
 const ImgCarousel = () => {
     return ( 
@@ -13,25 +20,79 @@ const ImgCarousel = () => {
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src={house}
+            src={house2}
             alt="First slide"
-            style={{ width: '100%', height: '100%' }} // add this line
+            style={{ width: '100%', height: '100%' }} 
             />
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src={swan}
+            src={house1}
             alt="Second slide"
-            style={{ width: '100%', height: '100%' }} // add this line
+            style={{ width: '100%', height: '100%' }} 
             />
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src={house}
+            src={house3}
             alt="Third slide"
-            style={{ width: '100%', height: '100%' }} // add this line
+            style={{ width: '100%', height: '100%' }} 
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={house4}
+            alt="Fourth slide"
+            style={{ width: '100%', height: '100%' }} 
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={house5}
+            alt="Fifth slide"
+            style={{ width: '100%', height: '100%' }} 
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={house6}
+            alt="Sixth slide"
+            style={{ width: '100%', height: '100%' }} 
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={house7}
+            alt="Seventh slide"
+            style={{ width: '100%', height: '100%' }} 
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={house8}
+            alt="Eighth slide"
+            style={{ width: '100%', height: '100%' }} 
+            />
+        </Carousel.Item>
+
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={house9}
+            alt="Ninth slide"
+            style={{ width: '100%', height: '100%' }} 
             />
         </Carousel.Item>
         </Carousel>
