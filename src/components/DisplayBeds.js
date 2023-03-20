e2bac5fd81608c1ba0aaeb3c9c6139fdaabde523
cDev-Creator <mccoyng170@gmail.com>
2023-03-20 14:19:01 -0500

    beds avaliable/bath/bedrooms

--- src/components/DisplayBeds.js
+++ src/components/DisplayBeds.js
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { FaBed, FaBath } from 'react-icons/fa';
+import { BsDoorOpenFill } from 'react-icons/bs';
 
-const DisplayBeds = () => {
+const DisplayBeds = (rat,rat1) => {
     const [bedsData, setBedsData] = useState([{}]);
-
     useEffect(() => {
         fetch("http://localhost:5000/bedCount")
             .then((response) => response.json())
             .then((data) => setBedsData(data));
     }, []);
-
-    console.log(bedsData);
-
     return (
         <div>
-            {typeof bedsData.beds === 'undefined' ? (
+            {typeof bedsData.beds === "undefined" ? (
                 <p>Loading...</p>
             ) : (
                 bedsData.beds.map((bed, i) => (
-                    <h2 key={i}> We have {bed} rooms available!</h2>
+                    <div key={i}>
+                    <p> <FaBed />{bed} beds available <FaBath/> 3 bath <BsDoorOpenFill/>4 bedrooms</p>
+                   {/*  <p>{bed}</p> */}
+                    </div>
+
+                 
                 ))
             )}
         </div>
