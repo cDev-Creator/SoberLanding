e2e24d68935f061f1563fb44ccc1e266a18f3522
cDev-Creator <mccoyng170@gmail.com>
2023-03-20 18:56:12 -0500

    footer 2.0

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,51 @@
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { BsFillPhoneFill, BsFillPersonFill } from 'react-icons/bs';
+import { MdLocationOn } from 'react-icons/md'
+import { AiFillMail } from 'react-icons/ai'
+
 const Footer = () => {
     return ( 
-        <section class="p-5">
-            <Container>
-            <div class="row g-4">
-          <div class="col-md">
-            <h2 class="text-center mb-4" >Sober Landing</h2>
-            <ul class="list-group list-group-flush lead">
-            <li class="list-group-item">
-                <span class="fw-bold">House Manager:</span> Landyn Jennings
-              </li>
-              <li class="list-group-item">
-                <span class="fw-bold">Our Location:</span> 503 Main Street San Antonio TX
-              </li>
-              <li class="list-group-item">
-                <span class="fw-bold">Main Phone:</span> (555) 555-5555
-              </li>
-              <li class="list-group-item">
-                <span class="fw-bold">Email Address:</span> (555)
-                test@rand.test
-              </li>
-           
-            </ul>
-          </div>
-          <div class="col-md">
-          <iframe style={{width:'100%', height:'100%'}} src="https://maps.google.com/maps?q=6716%20riverwood%20live%20oak%20texas&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-          </div>
-        </div>
+        <section className="p-4"> 
+          <Container>
+          <Row className="justify-content-center">
+          <Col sm={2} className="d-flex align-items-center justify-content-center">
+            <Col>
+              <h2 className="mb-4">Sober Landing</h2>
+              <p>To support women in recovery from alcohol and other drugs.</p>
+            </Col>
+          </Col>
 
-            </Container>
-        </section> 
-     );
+          <Col sm={4} className="d-flex align-items-center justify-content-center">
+            <ul className="list-group list-group-flush lead text-center">
+              <li className="list-group-item d-flex align-items-center">
+                <span className="fw-bold me-2"><BsFillPersonFill /></span> Landyn Jennings
+              </li>
+              <li className="list-group-item d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center">
+                  <span className="fw-bold me-2"><Row><MdLocationOn /></Row></span>
+                  <div className="text-start">
+                    6716 Riverwood<br />
+                    Live Oak, TX 78233
+                  </div>
+                </div>
+              </li>
+              <li className="list-group-item d-flex align-items-center">
+                <span className="fw-bold me-2"><BsFillPhoneFill /></span> (555) 555-5555
+              </li>
+              <li className="list-group-item d-flex align-items-center">
+                <span className="fw-bold me-2"><AiFillMail /></span> test@rand.test
+              </li>
+            </ul>
+          </Col>
+
+          <Col sm={6} style={{height:'350px'}}>
+            <iframe style={{width:'100%', height:'100%'}} src="https://maps.google.com/maps?q=6716%20riverwood%20live%20oak%20texas&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
+          </Col>
+          </Row>
+            
+          </Container>
+      </section> 
+    );
 }
  
 export default Footer;
