93dbbe4f5ca10ab0bc86b71faed8ecf3ed8e2fa7
cDev-Creator <mccoyng170@gmail.com>
2023-03-23 17:42:33 -0500

    quote display altered

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,27 +1,29 @@
 import { Container } from "react-bootstrap";
 import bg from '../assets/images/bg.jpg';
 
+
 var sectionStyle = {
 /*   backgroundImage: `url(${bg})`,
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
   backgroundPosition: 'center',  */
 }
+const blue = {color:'#5c8cac'};
+
+
 
 const Quote = () => {
   return ( 
-    <section className="p-4"style={sectionStyle}>
-      <Container >
-
-        <div style={{backgroundColor:'#333333ef' ,borderRadius:'30px', border: '1px solid #333' }}>
-        <h1 className="pt-5 px-2 text-light text-center title">"The fist step towards getting somewhere is to decide you're not going to stay where you are"</h1>
-        <h3 className="pb-5 text-light text-center title">- J.P. Morgan</h3>
-     
-        </div>
-   
-      </Container>
-      
-    </section>
+    <div>
+ <section className="p-4"/* style={sectionStyle} */>
+          <Container style={{borderRadius: '24% 76% 35% 65% / 46% 29% 71% 54%'}}>
+            <div className='p-1'>
+            <h1 className="px-2 title text-md-center text-sm-left">"The fist step towards getting somewhere is to decide you're not going to stay where you are"</h1>
+            <h3 className="pb-5 pt-4 title text-md-center text-sm-left" style={blue}>- J.P. Morgan</h3>
+            </div>
+          </Container>
+        </section>
+    </div>
   );
 }
 
