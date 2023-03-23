4a0d109f7be64cbd8fa7b208fe4b522ff5a2f26b
cDev-Creator <mccoyng170@gmail.com>
2023-03-22 23:35:48 -0500

    quote styled

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,21 +2,25 @@ import { Container } from "react-bootstrap";
 import bg from '../assets/images/bg.jpg';
 
 var sectionStyle = {
-  backgroundImage: `url(${bg})`,
+/*   backgroundImage: `url(${bg})`,
   backgroundSize: 'cover',
   backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center'
+  backgroundPosition: 'center',  */
 }
 
 const Quote = () => {
   return ( 
-    <section className="p-5"style={sectionStyle}>
+    <section className="p-4"style={sectionStyle}>
       <Container >
-        <div style={{backgroundColor:'#3f4f6299'}}>
-        <h1 className="pt-5 text-light text-center">"The fist step towards getting somewhere is to decide you're not going to stay where you are"</h1>
-        <h3 className="pb-5 text-light text-center ">- J.P. Morgan</h3>
+
+        <div style={{backgroundColor:'#333333ef' ,borderRadius:'30px', border: '1px solid #333' }}>
+        <h1 className="pt-5 px-2 text-light text-center title">"The fist step towards getting somewhere is to decide you're not going to stay where you are"</h1>
+        <h3 className="pb-5 text-light text-center title">- J.P. Morgan</h3>
+     
         </div>
+   
       </Container>
+      
     </section>
   );
 }
