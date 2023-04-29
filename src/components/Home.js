89d5fdd6d6120d0775ae440a7e7fdd46d2feb05b
cDev-Creator <mccoyng170@gmail.com>
2023-04-28 20:05:41 -0500

    wording updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,11 +5,10 @@ import Copyright from './Copyright';
 import { Container, Col, Row } from 'react-bootstrap';
 import { FaCheck } from 'react-icons/fa';
 
-import house from '../assets/images/9.jpg';
+import house from '../assets/images/outside.jpg';
 const icons = {fill:'#d4cdc4', marginRight: '10px'};
 const blue = {color:'#5c8cac'};
 const grey = '#f3f4f5';
-const tan = {color:'#ac7c5c'}
 
 const Home = () => {
     return (  
@@ -20,13 +19,13 @@ const Home = () => {
                 <Container >
                 <div className='m-4 pt-5'>
                     <div className="text-center d-none d-md-block">
-                        <h2 className='font-md font-big' style={blue}>Are you seeking a supportive community to aid you in your new sober lifestyle?</h2>
-                        <p>If you're a woman seeking a safe and supportive environment to help you in your journey towards sobriety, look no further than Sober Landing. Our mission is to provide you with the tools and resources necessary to apply the skills and knowledge gained during treatment to your daily life.</p>
+                        <h2 className='font-md font-big mb-3' style={blue}>What are you seeking in your sobriety?</h2>
+                        <p>If you're a woman seeking a safe and supportive environment to help you in your journey towards sobriety, look no further than Sober Landing. Our goal is to provide a strong community of recovery for those who are seeking to achieve lasting sobriety.</p>
                     </div>
                   
-                    <div className='d-block d-md-none'>
-                        <h2 className='font-big' style={blue}>Are you seeking a supportive community to aid you in your new sober lifestyle?</h2>     
-                        <p >If you're a woman seeking a safe and supportive environment to help you in your journey towards sobriety, look no further than Sober Landing. Our mission is to provide you with the tools and resources necessary to apply the skills and knowledge gained during treatment to your daily life.</p>   
+                    <div className='d-block d-md-none mb-3'>
+                        <h2 className='font-big' style={blue}>What are you seeking in your sobriety?</h2>     
+                        <p >If you're a woman seeking a safe and supportive environment to help you in your journey towards sobriety, look no further than Sober Landing. Our goal is to provide a strong community for those who are seeking to achieve lasting sobriety.</p>   
                     </div>  
                     </div>  
               
@@ -43,18 +42,18 @@ const Home = () => {
                         <Col>
                         <h2 className='pt-5 font-big'>Our Housing</h2>
                         <p style={{maxWidth:'400px'}}>Our sober housing offers a wide range of amenities to ensure that you feel comfortable and at home during your stay with us.</p>
-                        <ul style={{ listStyleType: 'none', paddingLeft: 0 }}>
-                            <li> <FaCheck style={icons} /> Fully equipped kitchen with appliances and cooking utensils</li>
-                            <li> <FaCheck style={icons} /> Shared living areas with comfortable furniture and TV</li>
-                            <li> <FaCheck style={icons} /> High-speed internet and Wi-Fi access</li>
-                            <li> <FaCheck style={icons} /> Laundry room with washing machine and dryer</li>
-                            <li> <FaCheck style={icons} /> Outdoor space including a patio and spacious backyard </li>
-                            <li> <FaCheck style={icons} /> Access to 12-step meetings and other recovery programs</li>
+                        <ul style={{ listStyleType: 'none', paddingLeft: 0, marginTop:'40px' }}>
+                            <li> <FaCheck style={icons} className='icons'/> Fully equipped kitchen with appliances and cooking utensils</li>
+                            <li> <FaCheck style={icons} className='icons' /> Shared living areas with comfortable furniture and TV</li>
+                            <li> <FaCheck style={icons} className='icons' /> High-speed internet and Wi-Fi access</li>
+                            <li> <FaCheck style={icons} className='icons'/> Laundry room with washing machine and dryer</li>
+                            <li> <FaCheck style={icons} className='icons'/> Outdoor space including a patio and spacious backyard </li>
+                            <li> <FaCheck style={icons} className='icons'/> Access to 12-step meetings and other recovery programs</li>
                         </ul>
                         </Col>
 
                         <Col md="auto" className="d-flex align-items-center justify-content-md-center">
-                            <img src={house} alt="image of home" className="img-fluid w-75 w-sm-50 d-none d-sm-block" style={{ maxWidth: '550px', borderRadius: '90% 10% 87% 13% / 14% 87% 13% 86%', opacity:'0.9'}} />
+                            <img src={house} alt="image of home" className="img-fluid w-75 w-sm-50 d-none d-sm-block" style={{ maxWidth: '550px', borderRadius:'49% 51% 52% 48% / 54% 55% 45% 46% ', opacity: '0.9', border: '4px solid #d4cdc4'}} />
                         </Col>
                         </Row>
                    </Container>
@@ -67,22 +66,14 @@ const Home = () => {
 
                    <Container className="my-5 p-4" >
                         <h2 className='font-big' style={blue} >Develop Lasting Realtionships</h2>
-                        <p>We understand the importance of developing meaningful relationships with other women who are also on the path towards recovery. Our residents are encouraged to attend meetings and form bonds with other women who understand the struggles of maintaining sobriety. By supporting one another, we help each other progress towards a healthier and more fulfilling life.</p>
-                        <h1>MOD</h1>
-                        <p>The life we once lived can be isolating, which can create a lot of fear and loneliness in the start of your recovery. Being in sober living is a way to slowly acclimate yourself to making new friends and being part of a community. By maintaining these relationships you can help support one another. </p>
+                        <p>We understand the importance of developing meaningful relationships with other women who are also on the path towards recovery. Sober Landing offers a way to gradually adapt to building new friendships and being part of a community. During your stay with us, you will attend meetings and events to form bonds with other women who understand the struggles of maintaining sobriety.</p>
 
-                        <h4 className='font-md pt-5'>We Want to Help</h4>  
-{/*                         <p>At Sober Landing, you'll have access to nearby meetings, employment opportunities, sponsors, and new sober friends who are all committed to helping you succeed. We understand the importance of routine and work hard to create a safe and supportive environment while prioritizing recovery.</p>   
- */}            
-                        <p>At Sober Landing, you'll have access to nearby meetings, employment opportunities, sponsors, and new friends also in recovery. While understanding the importance of routine, we work hard to create a safe and supportive environment while prioritizing recovery.</p>   
+                        <h4 className='font-md pt-5'>We Want to Help</h4>       
+                        <p>At Sober Landing, you'll have access to nearby meetings, employment opportunities, sponsors, and new friends also in recovery. While understanding the importance of routine, we work hard to create a safe and supportive environment that prioritizes recovery.</p>   
                         <p>Don't hesitate to contact us! We would love to show you around our home and figure out whether our sober living accommodations are a good match for you. Together, we can assist you in staying sober and continuing on the path to recovery. We're eager to provide you with support throughout your journey!</p>   
 
                 </Container>
             </section> 
-
-
-{/*         < Quote /> */}
-    
         < Footer />
         < Copyright />
 
