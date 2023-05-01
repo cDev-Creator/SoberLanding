1840eecb4fe84366c15c31657159205778e1a84c
mothmatrix <76753452+cDev-Creator@users.noreply.github.com>
2023-04-30 22:15:33 -0500

    bed display updated properly

--- client/src/components/DisplayBeds.js
+++ client/src/components/DisplayBeds.js
@@ -3,26 +3,26 @@ import { FaBed, FaBath } from 'react-icons/fa';
 import { BsDoorOpenFill } from 'react-icons/bs';
 const iconColor ='#5c8cac';
 
-const DisplayBeds = (rat,rat1) => {
+const API_URL = "https://sl3.onrender.com/bedcount";
+
+const DisplayBeds = () => {
     const [bedsData, setBedsData] = useState([{}]);
     useEffect(() => {
-        fetch("http://localhost:5000/bedCount")
+        /* fetch("http://localhost:5000/bedCount") */
+        fetch("https://sl3.onrender.com/bedcount")
             .then((response) => response.json())
             .then((data) => setBedsData(data));
     }, []);
     return (
         <div>
-            {typeof bedsData.beds === "undefined" ? (
-                <p>Loading...</p>
-            ) : (
-                bedsData.beds.map((bed, i) => (
-                    <div key={i}>
-                    <p> <FaBed fill={iconColor}/> {bed} beds available <FaBath fill={iconColor}/> 2.5 bath <BsDoorOpenFill fill={iconColor}/>4 bedrooms</p>
-                   {/*  <p>{bed}</p> */}
-                    </div>
-                ))
-            )}
-        </div>
+        {typeof bedsData.beds === "undefined" ? (
+            <p>Loading...</p>
+        ) : (
+            <div>
+                <p> <FaBed fill={iconColor}/> {bedsData.beds} beds <FaBath fill={iconColor}/> 2.5 bath <BsDoorOpenFill fill={iconColor}/>4 bedrooms</p>
+            </div>
+        )}
+    </div>
     );
 };
 
